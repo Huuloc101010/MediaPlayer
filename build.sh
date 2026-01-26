65801b8b3d51bd371cc584207892dbf7a1854ec4
lochh2 <huuloc101010@gmail.com>
2026-01-26 23:00:41 +0700

    add c++20 for this project

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 # This is temp build script, this script will be refactor
 
 #!/bin/bash
-g++ main.cpp `pkg-config --cflags --libs libavformat libavcodec libavutil` -o a.out
+g++ \
+main.cpp \
+--std=c++20 `pkg-config --cflags --libs libavformat libavcodec libavutil` -o a.out
 
 echo "Build code success"
