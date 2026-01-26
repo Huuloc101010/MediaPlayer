8ed1a943da915605cb6ad1b372d5b7908d319f9b
lochh2 <huuloc101010@gmail.com>
2026-01-26 23:39:55 +0700

    init class player

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 # This is temp build script, this script will be refactor
 
 #!/bin/bash
-g++ \
-main.cpp \
---std=c++20 `pkg-config --cflags --libs libavformat libavcodec libavutil` -o a.out
-
+#g++ player.cpp \
+#main.cpp \
+#player.cpp \
+#--std=c++20 `pkg-config --cflags --libs libavformat libavcodec libavutil` -o a.out
+g++ --std=c++20 player.cpp \
+    `pkg-config --cflags --libs libavformat libavcodec libavutil` \
+    -o a.out
 echo "Build code success"
---
+++ player.cpp
@@ -0,0 +1,8 @@
+#include "player.h"
+
+int main(int argc, char** argv)
+{
+    player lplayer;
+    lplayer.run(argc, argv);
+    return 0;
+}
